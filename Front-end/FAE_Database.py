d00cbddbbb13de9c2d1c84c880b6f153a12f748a
Daniel Taufiq <Daniel-Taufiq@users.noreply.github.com>
2020-12-23 16:42:41 -0600

    added Treeview for table

--- Front-end/FAE_Database.py
+++ Front-end/FAE_Database.py
@@ -11,10 +11,14 @@ def home_page(root, title):
     root.geometry('750x500+300+100')                            # Open window in center of screen
     root.minsize(500, 350)
 
+    
+
 def createTabs(tabControl, tab1, tab2):
     tabControl.add(tab1, text='Tab 1')
     tabControl.add(tab2, text='Tab 2')
     tabControl.pack(expand = 1, fill="both")
+
+    
 
 def populateTable(tab1, data):
     total_rows = len(data)          # number of rows
@@ -22,18 +26,39 @@ def populateTable(tab1, data):
     print(data)
     print(total_rows)
     print(total_columns)
-    total_rows = 20
-    total_columns = 10
+    #total_rows = 20
+    #total_columns = 10
+    print("Number of widgets: ", total_rows * total_columns)
+    area=('Phase', 'Blue', 'Red', 'Score', 'Patch', 'BB1', 'RB1', 'BB2', 'RB3', 'BP1', 'RP1-2', 'BP2-3', 'RP3', 'RB4', 'BB4', 'RB5', 'BB5', 'RP4', 'BP4-5', 'RP5')
+    ac=('all','n','o','s','ne','nw','sw','m', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l')
+    tv=ttk.Treeview(tab1,columns=ac,show='headings', height=50)
+    for i in range(len(area)):
+        #print(area[i])
+        if area[i] == 'Score':
+            tv.column(ac[i],width=50,anchor='e')
+            
+        elif area[i] == 'Patch':
+            tv.column(ac[i],width=50,anchor='e')
+        else:
+            tv.column(ac[i],width=95,anchor='e')
+        tv.heading(ac[i],text=area[i])
+    tv.pack()
 
-    for i in range(total_rows):
-        for j in range(total_columns):
-            e = Entry(tab1, width=4, fg='black', font=('Arial',9,'bold'), 
-                        bd=2, justify="center")         # bd = border size   
-            e.grid(row=i, column=j, sticky="NSEW")
-            tab1.grid_columnconfigure(j, weight=1)  # set scaling for columns
-            tab1.grid_rowconfigure(i, weight=1)     # set scaling for rows
-            e.insert(END, data[i][j])               # insert the data
-            e.config(state="readonly")
+    for i in range(76):
+        tv.insert('','end',values=data[i])
+
+
+
+    # for i in range(total_rows):
+    #     for j in range(total_columns):
+    #         e = Entry(tab1, width=10, fg='black', font=('Arial',9,'bold'), 
+    #                     bd=2, justify="center")         # bd = border size   
+    #         e.grid(row=i, column=j, sticky="NSEW")
+    #         #tab1.grid_columnconfigure(j, weight=1)  # set scaling for columns
+    #         #tab1.grid_rowconfigure(i, weight=1)     # set scaling for rows
+    #         e.insert(END, data[i][j])               # insert the data
+    #         e.config(state="readonly")
+
 
 def getPickBanData(pickAndBanOrder):
     url1 = requests.get("https://lol.gamepedia.com/Special:RunQuery/PickBanHistory?PBH%5Bpage%5D=Worlds+2020+Main+Event&PBH%5Btextonly%5D=Yes&pfRunQueryFormName=PickBanHistory")
